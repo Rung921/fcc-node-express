8678db0ad79617c613bbeb19c81ccfb8fc4a7bb2
rung <20408348+Rung921@users.noreply.github.com>
2025-02-25 20:30:41 -0800

    .env file created

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 let express = require('express');
-const path = require('path');
+let path = require('path');
+let dotenv = require('dotenv').config()
+
 let app = express();
 
 const absolutePath = __dirname + "/views/index.html";
@@ -15,7 +17,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/json', (req, res) => {
-    res.json({ "message" : "Hello json" })
+    const msg = "Hello json"
+    res.json({ "message" : process.env.MESSAGE_STYLE === "uppercase" ?
+        msg.toUpperCase() :
+        msg
+     });
 })
 
  module.exports = app;
