f244cf28c3685a26ccd814b4b7541c1641666c67
rung <20408348+Rung921@users.noreply.github.com>
2025-02-25 19:25:31 -0800

    Added JSON message

--- myApp.js
+++ myApp.js
@@ -1,48 +1,21 @@
 let express = require('express');
+const path = require('path');
 let app = express();
 
 const absolutePath = __dirname + "/views/index.html";
 const str = "Hello Express"
 
 console.log(absolutePath);
+console.log(path.join(__dirname, 'public'))
 
-app.use("/public", express.static(__dirname + "/public"))
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.sendFile(absolutePath)
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/json', (req, res) => {
+    res.json({ "message" : "Hello json" })
+})
 
  module.exports = app;
